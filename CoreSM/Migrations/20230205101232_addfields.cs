0a017a5c776a1078d53f513e82efdea22f95ec29
Owais <champsowaiz@hotmail.com>
2023-02-18 15:11:34 +0500

    fist cmt

---
+++ CoreSM/Migrations/20230205101232_addfields.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreSM.Migrations
+{
+    public partial class addfields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Invoice",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserID = table.Column<int>(nullable: false),
+                    InvoiceDate = table.Column<DateTime>(nullable: false),
+                    CNIC = table.Column<string>(nullable: true),
+                    TaxPayerType = table.Column<string>(nullable: true),
+                    SaleType = table.Column<string>(nullable: true),
+                    SaleCategoryID = table.Column<int>(nullable: false),
+                    TaxRate = table.Column<decimal>(nullable: false),
+                    TaxWithheld = table.Column<string>(nullable: true),
+                    TaxAmount = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoice", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payment",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserID = table.Column<int>(nullable: false),
+                    PaymentDate = table.Column<DateTime>(nullable: false),
+                    CNIC = table.Column<string>(nullable: true),
+                    TaxPayerType = table.Column<string>(nullable: true),
+                    SaleType = table.Column<string>(nullable: true),
+                    SaleCategoryID = table.Column<int>(nullable: false),
+                    TaxRate = table.Column<decimal>(nullable: false),
+                    TaxWithheld = table.Column<string>(nullable: true),
+                    TaxAmount = table.Column<decimal>(nullable: false),
+                    isCreated = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalesCategory",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Type = table.Column<string>(nullable: true),
+                    PayerType = table.Column<string>(nullable: true),
+                    Category = table.Column<string>(nullable: true),
+                    TaxRate = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesCategory", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    IsInvoice = table.Column<bool>(nullable: false),
+                    IsPayment = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Invoice");
+
+            migrationBuilder.DropTable(
+                name: "Payment");
+
+            migrationBuilder.DropTable(
+                name: "SalesCategory");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
